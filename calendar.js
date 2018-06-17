36cd6185f3cffd70d3ca030c36962b9c702bb39f
jonathan <jonathan.araya.m@gmail.com>
2018-06-17 10:13:46 -0400

    Calendar with colors

--- calendar.js
+++ calendar.js
@@ -1,71 +1,25 @@
-var codes = new CountryCodes();
-var dayNames = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-
-function generateCalendar() {
-    let tempDate = document.getElementById("startdate").value,
-        numberOfDays = parseInt(document.getElementById("numberofdays").value),
-        countryCode = document.getElementById("countrycode").value;
-    // startDate = new Date(startDate);
-    if (!countryCode || !tempDate || !numberOfDays) {
-        alert('Data required');
-        return;
-    }
-    if (typeof countryCode === "string" && countryCode.length) {
-        country = codes.get(countryCode);
-        if (!country) {
-            alert('Invalid Country Code');
-        } else {
-            // document.write(country.name);
-            // document.write(country.code);
-        }
-    }
-    let [startYear, startMonth, startDay] = [...tempDate.split('-').map(td => parseInt(td))];
-    let startDate = moment().year(startYear).month(startMonth - 1).date(startDay);
-    let finishDate = moment(startDate).add(numberOfDays, 'd');
-    // let [finishYear, finishMonth, finishDay] = [finishDate.year(), finishDate.month(), finishDate.date()];
-
-    let daysQuantity = parseInt((finishDate).diff(startDate, 'days', true));
-
-    let days = [];
-    let weeks = [];
-    let months = [];
-
-    let currentDate = moment(startDate);
-    let daysCounter = 1;
-    let weeksQuantity = ((startDate.day() + numberOfDays) / 7);
-
-    for (let i = 0; i < weeksQuantity; i++) {
-        let currentMonth = currentDate.month();
-        let weekPosition = currentDate.weekday();
-        days = weekPosition === 0 ? [] : Array.from(Array(weekPosition), (_, x) => '');
-        for (let j = weekPosition; j < 7; j++) {
-            if (currentDate.month() !== currentMonth || daysCounter > daysQuantity) {
-                days.push('');
-            } else {
-                // days.push(currentDate.date());
-                days.push(moment(currentDate));
-                currentDate.add(1, 'd');
-                daysCounter += 1;
-            }
-        }
-        weeks.push(days);
-        if (currentDate.date() === 1) {
-            months.push(weeks);
-            weeks = [];
-        }
-    }
-    if (currentDate.date() !== 1) {
-        months.push(weeks);
-    }
-
-    console.table(months);
-    generateView(months);
-    // let week = document.createElement('div');
-}
+const codes = new CountryCodes();
+const holidays = new Holidays();
+const dayNames = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+var tempHolidays;
+var numberOfDays;
 
 function createDay(date) {
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode(moment.isMoment(date) ? date.date() : ' '));
+    if (moment.isMoment(date)) {
+        li.appendChild(document.createTextNode(date.date()));
+        let mmdd = date.format('MM') + '-' + date.format('DD');
+        if (tempHolidays.days[mmdd]) {
+            li.classList.add("holiday");
+        } else if (date.day() === 0 || date.day() === 6) {
+            li.classList.add("weekend");
+        } else {
+            li.classList.add("weekday");
+        }
+    } else {
+        li.appendChild(document.createTextNode('X'));
+        li.classList.add("notaday");
+    }
     return li;
 }
 function createWeek(week) {
@@ -78,15 +32,15 @@ function createWeek(week) {
 }
 
 function createMonth(month) {
-    //crear contenedor
+    //contenedor
     let div = document.createElement("div");
-    
+
     //titulo
     let ul = document.createElement("ul");
     ul.classList.add('month');
     let li = document.createElement("li");
     let thisMonth = month[0].find(m => moment.isMoment(m));
-    let title = thisMonth.format('MMMM') + ' - ' + thisMonth.format('YYYY');
+    let title = thisMonth.format('MMMM') + ' ' + thisMonth.format('YYYY');
     li.appendChild(document.createTextNode(title));
     ul.appendChild(li);
 
@@ -99,8 +53,8 @@ function createMonth(month) {
         ulDayNames.appendChild(liDayName);
     });
     // Agregar al DOM
-    div.appendChild(ul);
     div.appendChild(ulDayNames);
+    div.appendChild(ul);
     return div;
 }
 
@@ -109,10 +63,10 @@ function generateView(months) {
     var calendar = document.getElementById("calendar");
     if (calendar) {
         document.body.removeChild(calendar);
-        calendar = document.createElement("div");
-        calendar.setAttribute("id", "calendar");
-        document.body.appendChild(calendar);
-    } 
+    }
+    calendar = document.createElement("div");
+    calendar.setAttribute("id", "calendar");
+    document.body.appendChild(calendar);
     months.forEach(m => {
         let month = createMonth(m);
         m.forEach(w => {
@@ -120,4 +74,61 @@ function generateView(months) {
         });
         calendar.appendChild(month);
     });
+}
+
+function generateLogic(startDate, finishDate) {
+    let week = [];
+    let month = [];
+    let months = [];
+
+    let currentDate = moment(startDate);
+    let daysCounter = 1;
+
+    while (finishDate.diff(currentDate) > 0) {
+        let currentMonth = currentDate.month();
+        let weekPosition = currentDate.weekday();
+        week = weekPosition === 0 ? [] : Array.from(Array(weekPosition), (_, x) => '');
+        for (let j = weekPosition; j < 7; j++) {
+            if (currentDate.month() !== currentMonth || daysCounter > numberOfDays) {
+                week.push('');
+            } else {
+                week.push(moment(currentDate));
+                currentDate.add(1, 'd');
+                daysCounter += 1;
+            }
+        }
+        month.push(week);
+        if (currentDate.date() === 1) {
+            months.push(month);
+            month = [];
+        }
+    }
+    if (currentDate.date() !== 1) {
+        months.push(month);
+    }
+    return months;
+}
+
+
+function generateCalendar() {
+    let tempDate = document.getElementById("startdate").value,
+        countryCode = document.getElementById("countrycode").value;
+    numberOfDays = parseInt(document.getElementById("numberofdays").value);
+    if (!countryCode || !tempDate || !numberOfDays) {
+        alert('Data required');
+        return;
+    }
+    if (typeof countryCode === "string" && countryCode.length) {
+        country = codes.get(countryCode);
+        tempHolidays = holidays.get(countryCode);
+        if (!country) {
+            return alert('Invalid Country Code');
+        }
+    }
+    let [startYear, startMonth, startDay] = [...tempDate.split('-').map(td => parseInt(td))];
+    let startDate = moment().year(startYear).month(startMonth - 1).date(startDay);
+    let finishDate = moment(startDate).add(numberOfDays, 'd');
+
+    let months = generateLogic(startDate, finishDate);
+    generateView(months);
 }
